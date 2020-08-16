7f2173836cbfc37608743f6a70d802097b1ab11e
bkthomps <bkthomps@uwaterloo.ca>
2020-08-16 19:05:11 -0400

    Revert "Test valgrind auto-failure"
    This reverts commit 5407eaab56bab103d39141d9ed68d9833a013723.

--- src/unordered_map.c
+++ src/unordered_map.c
@@ -234,7 +234,7 @@ static char *unordered_map_create_element(unordered_map me,
                                           const void *const key,
                                           const void *const value)
 {
-    char *init = malloc(ptr_size + hash_size + me->value_size);
+    char *init = malloc(ptr_size + hash_size + me->key_size + me->value_size);
     if (!init) {
         return NULL;
     }
